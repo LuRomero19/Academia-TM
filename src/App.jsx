a64103d444d35d0892fd5feae885388f550e3faa
kaz <fabri.r.nm@gmail.com>
2026-09-17 22:10:24 -0300

    Se quitó el contador junto con su clase css

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Academia TM</h1>
+          <p>Acá debería haber un contador, pero ya no está...</p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
       </section>
 
       <div className="ticks"></div>
